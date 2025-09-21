a501652c97f1183e2690de484e318615a883f691
Ajay Jakate <ajay.jakate@gmail.com>
2025-09-20 19:45:15 -0500

    maybe this will work better

--- src/App.tsx
+++ src/App.tsx
@@ -23,21 +23,29 @@ function App() {
   console.log("geolocationEnabled", isGeolocationEnabled);
   console.log("geoLocationData", coords);
 
-  return !isGeolocationAvailable ? (
-    <div>Your browser does not support Geolocation</div>
-  ) : !isGeolocationEnabled ? (
-    <LoadingComponent debug={debug} />
-  ) : coords ? (
-    <MapComponent
-      latitude={truncateCoordinate(coords.latitude)}
-      longitude={truncateCoordinate(coords.longitude)}
-    />
-  ) : (
+  if (isGeolocationAvailable === false) {
+    return <div>Geolocation is not supported by your browser</div>;
+  }
+
+  if (isGeolocationEnabled === false) {
+    return  <LoadingComponent debug={debug} />;
+  }
+
+  if (coords) {
+    return (
+      <MapComponent
+        latitude={truncateCoordinate(coords.latitude)}
+        longitude={truncateCoordinate(coords.longitude)}
+      />
+    );
+  }
+
+  return (
     <div className="loading-main">
       <div style={{ marginBottom: "30px" }}>Fetching your location...</div>
       <Loader size="50px" />
     </div>
-  );
+  )
 }
 
 export default App
