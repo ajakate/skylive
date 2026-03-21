76282b1f8c3ba91881aa7f68266d5de1b59beae5
Ajay Jakate <ajay.jakate@gmail.com>
2026-03-20 21:20:33 -0500

    try pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'SkyLive',
+        short_name: 'SkyLive',
+        description: 'See airplanes around you',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'airplane.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any',
+          },
+        ],
+      },
+    }),
+  ],
   base: "https://ajakate.github.io/skylive/",
 })
